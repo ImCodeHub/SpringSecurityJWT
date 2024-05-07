11a8288e6832f3d7f03c25000aed8468033b2cb8
Ankit <ankitsharma.as420@gmail.com>
2024-05-07 23:49:43 +0530

    add getter and setter methods

--- src/main/java/com/Authentication/SpringSecurityJWT/Entity/User.java
+++ src/main/java/com/Authentication/SpringSecurityJWT/Entity/User.java
@@ -1,6 +1,4 @@
 package com.Authentication.SpringSecurityJWT.Entity;
-
-import org.hibernate.mapping.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
