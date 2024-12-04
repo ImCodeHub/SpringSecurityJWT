9e054066d94ed7ec5275edea23c83c9aa3e0161a
Ankit <ankitsharma.as420@gmail.com>
2024-12-04 14:44:16 +0530

    removed unwanted imports

--- src/main/java/com/Authentication/SpringSecurityJWT/Entity/User.java
+++ src/main/java/com/Authentication/SpringSecurityJWT/Entity/User.java
@@ -1,7 +1,6 @@
 package com.Authentication.SpringSecurityJWT.Entity;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
