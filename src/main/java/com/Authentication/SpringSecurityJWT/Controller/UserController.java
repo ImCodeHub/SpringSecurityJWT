76b854401de375c491d9e5d79fe769922db1f621
Ankit <ankitsharma.as420@gmail.com>
2024-12-04 22:23:13 +0530

    logout functionality implemented

--- src/main/java/com/Authentication/SpringSecurityJWT/Controller/UserController.java
+++ src/main/java/com/Authentication/SpringSecurityJWT/Controller/UserController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Authentication.SpringSecurityJWT.Auth.AuthService;
 import com.Authentication.SpringSecurityJWT.Auth.AuthenticationResponse;
+import com.Authentication.SpringSecurityJWT.Auth.TokenBlacklistService;
 import com.Authentication.SpringSecurityJWT.Model.AuthenticationRequest;
 import com.Authentication.SpringSecurityJWT.Model.RegisterRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// this is public URL end points that can access by anyone for registeration.
 @RestController
 @RequestMapping("/api/v1/auth")
 public class UserController {
@@ -24,6 +28,9 @@ public class UserController {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private TokenBlacklistService tokenBlacklistService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
@@ -39,4 +46,24 @@ public class UserController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+
+    // logout the user
+    // Client-Side Logout Implementation : in javaScript localStorage.removeItem('token');
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        String token = extractTokenFromRequest(request);
+        if (token != null) {
+            tokenBlacklistService.blacklistToken(token);
+        }
+        return ResponseEntity.ok("Logged out successfully.");
+    }
+
+    private String extractTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    
+    }
 }
